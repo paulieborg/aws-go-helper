403776b2bc597d883aae386c3112e425213d7268
Paul Borg <paul.borg@myob.com>
2017-04-28 10:46:01 +1000

    Tabs to Spaces via JetBrains

--- cloudformation.go
+++ cloudformation.go
@@ -1,103 +1,103 @@
 package main
 
 import (
-    "bufio"
-    "flag"
-    "fmt"
-    "os"
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
 
-    "encoding/json"
-    "io/ioutil"
+	"encoding/json"
+	"io/ioutil"
 
-    "github.com/aws/aws-sdk-go/aws"
-//  "github.com/aws/aws-sdk-go/aws/awserr"
-//  "github.com/aws/aws-sdk-go/aws/request"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/cloudformation"
+	"github.com/aws/aws-sdk-go/aws"
+	//  "github.com/aws/aws-sdk-go/aws/awserr"
+	//  "github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudformation"
 )
 
 type simpleParams map[string]string
 
 var (
-    name = flag.String("n", "", "Stack name.")
-    tmpl = flag.String("t", "network/template.yml", "Template file path.")
-    params = flag.String("p", "network/params.json", "Parameters file path.")
+	name   = flag.String("n", "", "Stack name.")
+	tmpl   = flag.String("t", "network/template.yml", "Template file path.")
+	params = flag.String("p", "network/params.json", "Parameters file path.")
 )
 
 var capability string = "CAPABILITY_NAMED_IAM"
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    t, err := getReader(*tmpl)
-    if err != nil {
-        panic(err)
-    }
-    raw_t, err := ioutil.ReadAll(t)
-    if err != nil {
-        panic(err)
-    }
+	t, err := getReader(*tmpl)
+	if err != nil {
+		panic(err)
+	}
+	raw_t, err := ioutil.ReadAll(t)
+	if err != nil {
+		panic(err)
+	}
 
-    p, err := getReader(*params)
-    if err != nil {
-        panic(err)
-    }
+	p, err := getReader(*params)
+	if err != nil {
+		panic(err)
+	}
 
-    cf_params, err := parseParams(p)
-    if err != nil {
-        panic(err)
-    }
+	cf_params, err := parseParams(p)
+	if err != nil {
+		panic(err)
+	}
 
-    sess := session.Must(session.NewSession())
-    svc := cloudformation.New(sess)
-    r, err := createStack(svc, cf_params, string(raw_t))
-    if err != nil {
-        panic(err)
-    }
+	sess := session.Must(session.NewSession())
+	svc := cloudformation.New(sess)
+	r, err := createStack(svc, cf_params, string(raw_t))
+	if err != nil {
+		panic(err)
+	}
 
-    fmt.Printf("%+v\n", r)
+	fmt.Printf("%+v\n", r)
 }
 
 func getReader(p string) (r *bufio.Reader, err error) {
-    f, err := os.Open(p)
-    if err != nil {
-        return
-    }
+	f, err := os.Open(p)
+	if err != nil {
+		return
+	}
 
-    return bufio.NewReader(f), nil
+	return bufio.NewReader(f), nil
 }
 
 func parseParams(b *bufio.Reader) (p []*cloudformation.Parameter, err error) {
-    var s_params simpleParams
-    err = json.NewDecoder(b).Decode(&s_params)
-    if err != nil {
-        return
-    }
+	var s_params simpleParams
+	err = json.NewDecoder(b).Decode(&s_params)
+	if err != nil {
+		return
+	}
 
-    for k, v := range s_params {
-        pkey := k
-        pval := v
-        p = append(p, &cloudformation.Parameter{
-            ParameterKey: &pkey,
-            ParameterValue: &pval,
-        })
-    }
+	for k, v := range s_params {
+		pkey := k
+		pval := v
+		p = append(p, &cloudformation.Parameter{
+			ParameterKey:   &pkey,
+			ParameterValue: &pval,
+		})
+	}
 
-    return
+	return
 }
 
 func createStack(svc *cloudformation.CloudFormation, p []*cloudformation.Parameter, t string) (r *cloudformation.CreateStackOutput, err error) {
-    stack := &cloudformation.CreateStackInput{
-        StackName: aws.String(*name),
-        Capabilities: []*string{
-            aws.String(capability),
-        },
-        Parameters: p,
-        TemplateBody: aws.String(t),
-        TimeoutInMinutes: aws.Int64(5),
-    }
+	stack := &cloudformation.CreateStackInput{
+		StackName: aws.String(*name),
+		Capabilities: []*string{
+			aws.String(capability),
+		},
+		Parameters:       p,
+		TemplateBody:     aws.String(t),
+		TimeoutInMinutes: aws.Int64(5),
+	}
 
-    return svc.CreateStack(stack)
+	return svc.CreateStack(stack)
 }
 
 //  if err != nil {
