0a553a0e99e417c25156aa4113ab24d223ab9fe2
Paul Borg <paul.borg@myob.com>
2017-05-06 06:31:35 +1000

    Update sed

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,7 +9,7 @@ echo '{
 go run main.go -n MyTestStack -a provision -b myob-dont-panic-test
 go run main.go -n MyTestStack -a provision -b myob-dont-panic-test
 
-sed -i '' 's#80#443#' templates/test-params.json
+sed -i 's/80/443/' templates/test-params.json
 
 go run main.go -n MyTestStack -a provision -b myob-dont-panic-test
 go run main.go -n MyTestStack -a delete
